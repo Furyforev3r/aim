48c1a04461e7e6e1ac532fd971381960308823e2
FuryForever <messiasfuryforever@gmail.com>
2025-04-24 23:13:42 -0300

    mobile: mobile header fixed

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -11,15 +11,13 @@
 
 <style>
     header {
-        min-width: 100%;
-        max-width: 100%;
+        width: 100%;
         display: flex;
         justify-content: space-between;
         align-items: center;
         padding: 1rem;
         background: var(--button-background);
         border-bottom: 1px solid var(--blue-primary-color);
-        flex-wrap: wrap;
     }
 
     h1 {
